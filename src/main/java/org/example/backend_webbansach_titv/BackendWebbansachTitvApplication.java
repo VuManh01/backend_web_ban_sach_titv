1a0abe1322d2377c25387f245bd226beceee2746
VuManh01 <vutrongmanhmk@gmail.com>
2025-04-09 22:20:33 +0700

    bai41

---
+++ src/main/java/org/example/backend_webbansach_titv/BackendWebbansachTitvApplication.java
@@ -0,0 +1,13 @@
+package org.example.backend_webbansach_titv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWebbansachTitvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWebbansachTitvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/backend_webbansach_titv/BackendWebbansachTitvApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backend_webbansach_titv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWebbansachTitvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
